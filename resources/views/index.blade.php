3fd6f06ea094d152625cc44bb3d52ead2da59cbf
oksa satya <oksasatyaa@gmail.com>
2022-02-17 19:31:20 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold subpixel-antialiased text-red-600 ">
+        Hello world!
+    </h1>
+    <div class="container mx-auto">
+        <div class="row">
+            <div class="col-start-2">
+                <button class="rounded-full bg-red-500 shadow-2xl font-normal text-sm-center"><span> Save
+                        Changes</span></button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    Button
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
